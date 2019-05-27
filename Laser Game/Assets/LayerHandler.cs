4bc40c6f1ac88c55fc3e7d91590d5744a101e745
Kavan Liang <kavan.liang@mail.utoronto.ca>
2019-05-27 01:29:48 -0400

    Did some work.
    Added skeleton code for enemy spawner and layer switching.

--- Laser Game/Assets/LayerHandler.cs
+++ Laser Game/Assets/LayerHandler.cs
@@ -2,17 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LayerHandler : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+public class LayerHandler : MonoBehaviour {
+    void Update() {
+		//TODO: Tweak this after we decide on spawning methods
+		this.gameObject.layer = (int) Mathf.Clamp(this.transform.position.y / GameManager.Instance.WorldHeight, 0, GameManager.Instance.NumLanes);
     }
 }
