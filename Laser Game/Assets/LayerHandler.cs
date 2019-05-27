443ec06542648447e0cc8b7228d546ae5f63490c
Kavan Liang <kavan.liang@mail.utoronto.ca>
2019-05-27 16:12:27 -0400

    Added Enemy Spawner
    Still need to add vamprie movement haha.

--- Laser Game/Assets/LayerHandler.cs
+++ Laser Game/Assets/LayerHandler.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 public class LayerHandler : MonoBehaviour {
     void Update() {
 		//TODO: Tweak this after we decide on spawning methods
-		this.gameObject.layer = (int) Mathf.Clamp(this.transform.position.y / GameManager.Instance.WorldHeight, 0, GameManager.Instance.NumLanes);
+		  gameObject.layer = 2;
     }
 }
---
+++ Laser Game/Assets/VampireController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VampireController : MonoBehaviour
+{
+    private float hp;
+    // Start is called before the first frame update
+    void Start()
+    {
+      hp = 100;
+    }
+
+    void decrementHp(float dmg) {
+      hp -= dmg;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(hp <= 0) {
+        Die();
+      }
+    }
+
+    void Die() {
+      //NOTIMPLEMENTEDYET
+    }
+}
