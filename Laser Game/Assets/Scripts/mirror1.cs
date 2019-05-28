b1d24e3abee1c07bf35ce4968db5effecc26056b
azengzzz <51031291+azengzzz@users.noreply.github.com>
2019-05-28 14:47:40 -0400

    mirrors

---
+++ Laser Game/Assets/Scripts/mirror1.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mirror1 : MonoBehaviour
+{
+    private Vector3 mouse_pos;
+    private Vector3 object_pos;
+    private float angle;
+    private bool selected = false;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && mirrorControll.selected1)
+        {
+            Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pz.z = 0;
+            gameObject.transform.position = pz;
+
+        }
+        if (Input.GetMouseButton(1) && mirrorControll.selected1)
+        {
+            mouse_pos = Input.mousePosition;
+            mouse_pos.z = -20;
+            object_pos = Camera.main.WorldToScreenPoint(transform.position);
+            mouse_pos.x = mouse_pos.x - object_pos.x;
+            mouse_pos.y = mouse_pos.y - object_pos.y;
+            angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        mirrorControll.selected1 = true;
+        mirrorControll.selected2 = false;
+    }
+}
---
+++ Laser Game/Assets/Scripts/mirrorControll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mirrorControll : MonoBehaviour
+{
+
+    public static bool selected1 = false;
+    public static bool selected2 = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
