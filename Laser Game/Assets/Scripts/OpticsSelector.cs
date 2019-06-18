30e26f74f86de8b15613020e2f341f43fcd3613a
Kavan Liang <kavan.liang@mail.utoronto.ca>
2019-06-18 10:05:59 -0400

    Revised the way that mirror selection works.

---
+++ Laser Game/Assets/Scripts/OpticsSelector.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpticsSelector : MonoBehaviour
+{
+	public static OpticsSelector Instance {
+		get;
+		private set;
+	}
+
+	public GameObject[] tools;
+
+	private Vector3 mouse_pos;
+	private Vector3 object_pos;
+	private float angle;
+	private GameObject selected;
+
+	private void reset() {
+		foreach(GameObject obj in tools) {
+			obj.SetActive(false);
+		}
+	}
+
+	private void Awake() {
+		if(Instance == null) {
+			Instance = this;
+			reset();
+			DontDestroyOnLoad(this.gameObject);
+		} else {
+			Destroy(this.gameObject);
+		}
+	}
+
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(Input.GetMouseButton(0)) {
+			Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			pz.z = 0;
+			if(selected) {
+				selected.transform.position = pz;
+			}
+		}
+		if(Input.GetMouseButton(1)) {
+			mouse_pos = Input.mousePosition;
+			mouse_pos.z = -20;
+			object_pos = Camera.main.WorldToScreenPoint(selected.transform.position);
+			mouse_pos.x = mouse_pos.x - object_pos.x;
+			mouse_pos.y = mouse_pos.y - object_pos.y;
+			angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+			if(selected) {
+				selected.transform.rotation = Quaternion.Euler(0, 0, angle);
+			}
+		}
+	}
+
+	public void selectTool(GameObject obj) {
+		selected = obj;
+	}
+}
---
+++ Laser Game/Assets/Scripts/Selectable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selectable : MonoBehaviour {
+	void OnMouseDown() {
+		OpticsSelector.Instance.selectTool(this.gameObject);
+	}
+}
