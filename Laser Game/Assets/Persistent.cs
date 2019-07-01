f00732ee41d3fa48ebcc1f825c76f27cc48e7774
Kavan Liang <kavan.liang@mail.utoronto.ca>
2019-07-01 16:32:48 -0400

    Added some sick background music.

---
+++ Laser Game/Assets/Persistent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Persistent : MonoBehaviour
+{
+    public static Persistent Instance {
+        get;
+        private set;
+    }
+
+    void Awake() {
+        if(Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        } else {
+            Destroy(this.gameObject);
+        }
+    }
+}
