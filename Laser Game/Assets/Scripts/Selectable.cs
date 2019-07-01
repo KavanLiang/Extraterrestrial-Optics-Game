e620061d5676277f494aa9c9a8f11b7540967858
Kavan Liang <kavan.liang@mail.utoronto.ca>
2019-07-01 13:32:44 -0400

    Changed mirror rotation to right click again.

--- Laser Game/Assets/Scripts/Selectable.cs
+++ Laser Game/Assets/Scripts/Selectable.cs
@@ -8,10 +8,13 @@ public class Selectable : MonoBehaviour
     public Renderer rend;
     private float prevAlpha;
     public float rotation_speed;
+    private bool selected;
+    private static bool existSelected = false;
 
     void Start()
     {
         prevAlpha = 0.60f;
+        selected = false;
     }
 
     void OnMouseEnter()
@@ -24,13 +27,28 @@ public class Selectable : MonoBehaviour
         toggleSelected();
     }
 
+    void FixedUpdate() {
+        if(!Input.GetMouseButton(1)) {
+            selected = false;
+            existSelected = false;
+        } else {
+            if(selected) {
+                Vector3 mouse_pos = Input.mousePosition;
+                mouse_pos.z = 0;
+                Vector3 object_pos = Camera.main.WorldToScreenPoint(transform.position);
+                mouse_pos.x = mouse_pos.x - object_pos.x;
+                mouse_pos.y = mouse_pos.y - object_pos.y;
+                float angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.Euler(0, 0, angle);
+            }
+        }
+    }
+
     void OnMouseOver()
     {
-        if (Input.mouseScrollDelta.y != 0)
-        {
-            float delta = Input.mouseScrollDelta.y;
-            float angle = transform.rotation.eulerAngles.z + delta * Time.deltaTime * rotation_speed;
-            transform.rotation = Quaternion.Euler(0, 0, angle);
+        if(Input.GetMouseButton(1) && !existSelected) {
+            selected = true;
+            existSelected = true;
         }
     }
 
